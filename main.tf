66b3f39c4580feae13996e9e84319bd87d8a714f
Biplab Karmakar <karma.bip@gmail.com>
2024-02-06 09:11:37 -0500

    added user_data to run the webserver

--- main.tf
+++ main.tf
@@ -5,6 +5,11 @@ resource "aws_instance" "example" {
     ami = "ami-0fb653ca2d3203ac1"
     instance_type = "t2.micro"
 
+    user_data = <<- EOF
+                #!/bin/bash
+                echo "Hello World!" > index.html
+                nohup busybox httpd -f -p 8080 &
+
     tags = {
         Name = "terraform-example"
     }
